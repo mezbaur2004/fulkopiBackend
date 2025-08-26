5e5d9fcfd15add3df1929c29dadc0de463619f20
mezbaur2004 <mezbaur2004@gmail.com>
2025-08-26 17:30:35 +0600

    productModel created

---
+++ src/model/productModel.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+const DataSchema=mongoose.Schema({
+    title:{type:String,required:true},
+    des:{type:String,required:true},
+    price:{type:Number,required:true},
+    discount:{type:Boolean,required:true},
+    discountPrice:{type:Number},
+    image:{type:String,required:true},
+    stock:{type:Boolean,required:true},
+    remarks:{type:String,required:true},
+    categoryID:{type:mongoose.Schema.Types.ObjectId,required:true},
+    brandID:{type:mongoose.Schema.Types.ObjectId,required:true}
+},{timestamps:true,versionKey:false});
+
+const ProductModel=mongoose.model('products',DataSchema);
+module.exports=ProductModel;
